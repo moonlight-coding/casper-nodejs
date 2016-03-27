f78eadbd6551d2870bff649444da274b6848f9c7
Pierre-Emmanuel Lallemant <pe.lallemant@hotmail.fr>
2016-03-28 01:57:52 +0200

    spawn casperjs

--- src/casper-child/index.js
+++ src/casper-child/index.js
@@ -1,2 +1,14 @@
 console.log("Hi, you spawned me :)");
 
+var casper = require('casper').create({/*,
+  verbose: true,
+  logLevel: "debug"*/
+});
+
+casper.start('http://google.com');
+
+casper.run(function() {
+  this.echo('casper run() callback called');
+
+  this.exit();
+});
--- src/casper-nodejs/create.js
+++ src/casper-nodejs/create.js
@@ -1,7 +1,34 @@
+var spawn = require('child_process').spawn;
+
+function stripTrailingNewLine(str) {
+  if(str.substr(-1) === "\n") {
+    return str.substr(0, str.length - 1);
+  }
+  return str;
+}
+
 function create(params) {
   // spawn a child with casperjs    
 
+  var process = spawn('casperjs', [__dirname + '/../casper-child/index.js']);
+
+  process.stdout.on('data', (data) => {
+    data = stripTrailingNewLine(data.toString('utf-8'));
+    console.log(`[casperjs]: ${data}`);
+  });
+
+  process.stderr.on('data', (data) => {
+    data = stripTrailingNewLine(data.toString('utf-8'));
+    console.log(`[casperjs err]: ${data}`);
+  });
+
+  process.on('close', (code) => {
+    console.log('child process exited with code ${code}');
+  });  
+
   return {
+    _process: process,
+
     start: function() {
       console.log('TO IMPLEMENT: start ');
     },
