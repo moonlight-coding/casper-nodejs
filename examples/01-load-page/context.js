abd7e660d6f8ead1b1e21536803e5c2ece712aab
Pierre-Emmanuel Lallemant <pe.lallemant@hotmail.fr>
2016-04-01 01:06:57 +0200

    fix

---
+++ examples/01-load-page/context.js
@@ -0,0 +1,26 @@
+/*
+  01-load-page
+
+  Load a 
+*/
+
+var casper_nodejs = require('../../index.js');
+
+var url = "http://google.com";
+
+var casper = casper_nodejs.create(url);
+
+casper.then(function executed_in_this_context() {
+  console.log("page loaded");
+});
+
+casper.then([function executed_in_casperjs_context() {
+  return server._event_received; //Object.keys(this);
+}], function executed_after_in_this_context(ret) {
+  console.log(ret );
+  casper.exit();
+});
+
+
+casper.run();
+
